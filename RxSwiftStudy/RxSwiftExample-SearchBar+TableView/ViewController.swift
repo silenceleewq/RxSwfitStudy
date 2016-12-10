89a75135cd22dd4cf29a8db218c8f38b209e6085
LZY <958246321@qq.com>
2016-12-10 15:56:23 +0800

    我特么也不知道写了点什么,反正就是看不懂

--- RxSwiftStudy/RxSwiftExample-SearchBar+TableView/ViewController.swift
+++ RxSwiftStudy/RxSwiftExample-SearchBar+TableView/ViewController.swift
@@ -7,27 +7,52 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController, UITableViewDataSource {
 
-    @IBOutlet weak var tableView: UITableViewCell!
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-    var shownCities = [String]() //Data source for UITableView
-    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // our mocked API data source 
     
+    var shownCities = [String]() //Data source for UITableView
+    
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // our mocked API data source
+    let disposeBag = DisposeBag()  // Bag of disposables to release them when view is being deallocated (protect against retain cycle)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+
+        searchBar
+            .rx_text // Observable property thanks to RxCocoa
+            .throttle(0.5, scheduler: MainScheduler.instance) // wait 0.5 for changes
+            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
+            .filter{ $0.characters.count > 0}// if  the new value is really new, filter for non-empty query.
+            .subscribeNext { [unowned self] (query) in // Here we will be notified of every new value
+                self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities.
+                self.tableView.reloadData() // And reload table view data.
+            }
+            .addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 //MARK: - tableView datasource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return shownCities.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+        cell.textLabel?.text = shownCities[indexPath.row]
         
+        return cell
     }
 
 }
--- RxSwiftStudy/RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/ViewController.swift
@@ -1,27 +1,41 @@
 //
 //  ViewController.swift
+//  Created by lirenqiang on 2016/12/9.
 //  RxSwiftStudy
 //
-//  Created by lirenqiang on 2016/12/9.
 //  Copyright © 2016年 Ninja. All rights reserved.
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
-    
     override func viewDidLoad() {
 
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        addInt(first: 3, second: 3) { (result) -> (String) in
+            
+            return result+"aaaa"
+        }
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addInt (first: Int, second: Int, completion:(_ result: String)->(String)){
+        
+        let result = completion("asd")
+        print(result)
     }
-
-
 }
+
+/*
+ 
+ 在解释 observeOn()和subscribeOn() 时, 
+ 第4点说: 如果我们之前没有调用observeOn，那么这边会在OtherScheduler发生，但是我们前面调用了OtherScheduler，所以这个动作还是会在主线程中调用。
+ 是不是应该为: 如果我们之前没有调用observeOn，那么这边会在OtherScheduler发生，但是我们前面调用了observeOn，所以这个动作还是会在主线程中调用。
+ */
 
